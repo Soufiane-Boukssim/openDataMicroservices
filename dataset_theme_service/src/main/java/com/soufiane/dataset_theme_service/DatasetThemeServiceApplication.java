ba4405594e033a7c5fb312b31239c48bc0790a1c
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-06-02 16:48:20 +0100

    commit 4: move all common dependencies to Shared Library microservice

--- dataset_theme_service/src/main/java/com/soufiane/dataset_theme_service/DatasetThemeServiceApplication.java
+++ dataset_theme_service/src/main/java/com/soufiane/dataset_theme_service/DatasetThemeServiceApplication.java
@@ -2,8 +2,14 @@ package com.soufiane.dataset_theme_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.soufiane.dataset_theme_service",
+        "com.soufiane.sharedlibrary"
+})
+
 public class DatasetThemeServiceApplication {
 
     public static void main(String[] args) {
