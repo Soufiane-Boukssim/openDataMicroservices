46cd931982838a27f318ad343f0830bc678f4a6c
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-06-02 14:09:50 +0100

    commit 3: create Shared Library microservice

---
+++ shared-library/src/test/java/com/soufiane/sharedlibrary/SharedLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soufiane.sharedlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
