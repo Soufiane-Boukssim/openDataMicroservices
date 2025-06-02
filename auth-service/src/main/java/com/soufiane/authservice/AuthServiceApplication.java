27bd56769904359bd4ac47526b5e7c0f0e65b74e
Soufiane Boukssim <soufianeboukssim8@gmail.com>
2025-06-02 16:57:04 +0100

    commit 4.1: update 4.1

--- auth-service/src/main/java/com/soufiane/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/soufiane/authservice/AuthServiceApplication.java
@@ -2,8 +2,14 @@ package com.soufiane.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+        "com.soufiane.authservice",
+        "com.soufiane.sharedlibrary"
+})
+
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
